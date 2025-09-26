1b00e490ed6423551f1bc0599a33c283ec3c8c66
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-09-26 11:19:38 +0700

    using ApplicationBuilder

--- app/core/bot_client.py
+++ app/core/bot_client.py
@@ -1,14 +1,15 @@
 import traceback
 from .config import settings
 from telegram import Bot, constants
-from telegram.ext import Application
+from telegram.ext import Application, ApplicationBuilder
 
 from app.log.logger_config import setup_logging
 
 logger = setup_logging(__name__)
 
 bot = Bot(token=settings.telegram_token)
-app_ptb = Application.builder().token(settings.telegram_token).build()
+app_ptb = ApplicationBuilder().token(settings.telegram_token).build()
+# app_ptb = Application.builder().token(settings.telegram_token).build()
 
 async def send_message(chat_id: int, text: str):
     logger.info(f"Sending message to chat_id {chat_id}: {text}")
